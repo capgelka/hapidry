703d3ba202d4e2a199a028c5d49304e94b5abba5
capgelka <ldyach@yandex.ru>
2016-02-24 03:50:48 +0300

    trying to solve problem with unicode sequence in json from response

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,11 @@
 --
 -- This library provides several ways to handle JSON responses
 
-{-# LANGUAGE DeriveGeneric, OverloadedStrings#-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, FlexibleContexts #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
 
 import Control.Lens ((&), (^.), (^?), (.~), set)
-import Data.Aeson (FromJSON, fromJSON, decode)
+import Data.Aeson (FromJSON, fromJSON, decode, eitherDecode)
 import Data.Aeson.Encode (encodeToTextBuilder, encodeToBuilder)
 import Data.Aeson.Lens (key, _String, _Integer)
 import Data.Map (Map)
@@ -14,9 +14,9 @@ import Data.Maybe (fromMaybe)
 import Data.Either
 import Data.Text (Text)
 import qualified Data.Text.Lazy as L (toStrict)
-import qualified Data.Text.Lazy.Encoding as LE (decodeLatin1)
+import qualified Data.Text.Lazy.Encoding as LE (decodeLatin1, decodeUtf8)
 import qualified Data.Text.Lazy.Builder as LB (toLazyText)
-import Data.Text.Encoding (decodeLatin1)
+import Data.Text.Encoding (decodeLatin1, decodeUtf8)
 import Data.Text.Read (decimal)
 import Data.Binary (encode)
 import qualified Data.ByteString.Base16 as B16 (encode)
@@ -26,11 +26,16 @@ import Data.ByteString.Builder (toLazyByteString)
 import qualified Crypto.Hash.MD5 as MD5
 import Control.Monad.Reader
 import qualified Data.ByteString.Lazy.Char8 as BL --(ByteString)
+import Numeric (showHex)
 
 import GHC.Generics (Generic)
 import qualified Control.Exception as E
 
 import Network.Wreq
+
+
+
+
 
 
 -- This Haskell type corresponds to the structure of a response body
@@ -82,6 +87,12 @@ keyHash pass key = decodeLatin1 $ B16.encode $ MD5.hash $ B.append key pass --ne
 
 -- apiGet :: IO (Response B.ByteString)
 -- sid :: Reader                  -- 
+ununicode = table where -- map (\x -> if x == fst table then snd table else x) where
+  table = zip letters rus
+  rus = "ЁёАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя"
+  --rus = "\1025\1105\1040\1041\1042\1043\1044\1045\1046\1047\1048\1049\1050\1051\1052\1053\1054\1055\1056\1057\1058\1059\1060\1061\1062\1063\1064\1065\1066\1067\1068\1069\1070\1071\1072\1073\1074\1075\1076\1077\1078\1079\1080\1081\1082\1083\1084\1085\1086\1087\1088\1089\1090\1091\1092\1093\1094\1095\1096\1097\1098\1099\1100\1101\1102\1103"
+  letters = map (\l -> B.append "\\u" $ B.pack l)
+                 "0x0401":"0x0451":(map (\x -> showHex x ) [0x0410 .. 0x044f])
 
 toOptions :: [(Text, Text)] -> Options
 toOptions x = defaults & foldr (\(x, y) p -> p . set (param x) [y]) id x -- (\x f-> defaults x . f)
@@ -94,14 +105,14 @@ apiGet env p = apiGet' env (toOptions p) where
         (Right x) -> apiGet'' $ params & param "sid" .~ [x] where -- & param "sid" .~ [x] where
             apiGet'' :: Options -> IO (Either Integer BL.ByteString)
             apiGet'' params = do
-                print params
+                -- print params
                 r <- getWith params "http://www.diary.ru/api"
-                print $ r ^? responseBody 
+                -- print $ r ^? responseBody 
                 -- print $ r ^? responseBody . key "result"
                 -- print $ r ^? responseBody . key "result" . _String
                 -- print $ r ^? responseBody . key "result" . _Integer
                 -- print $ r ^? responseBody . key "result" . _String
-                print $ r ^? responseBody . _String
+                -- print $ r ^? responseBody . _String
                 case r ^? responseBody . key "result" . _String of
                    (Just "0")  -> return $ Right $ r ^. responseBody
                    (Just "12") -> authRequest e >>= (\newEnv -> apiGet' newEnv params) 
@@ -112,12 +123,20 @@ userGet :: ClientCredentials -> [(Text, Text)] -> IO (Either Integer BL.ByteStri
 userGet env params = apiGet env (("method", "user.get"):params)
 
 
-umailGet :: ClientCredentials -> [(Text, Text)] -> IO (Either Integer (Maybe Umail))
+--umailGet :: ClientCredentials -> [(Text, Text)] -> IO (Either Integer (Maybe Umail))
 umailGet env params = do
     r <- apiGet env (("method", "umail.get"):params)
-    return $ case r of
-      (Right x) -> Right $ (decode x :: Maybe Umail)
-      (Left x) -> Left $ x
+    x <- return $ case r of
+          (Right a) -> a
+          (Left a)  -> ""
+    return $ LE.decodeUtf8 $ x
+    -- return $ case r of
+    --   (Right x) -> Right $ (decode x :: Maybe Umail )-- >>= (\u -> u & umail >>= return)
+    --              -- do :: Maybe
+    --              --  u <- decode x
+    --              --  return $ u & umail 
+    --   --(x & eitherDecode :: Either Umail) & "umail" -- Right $ decode x  -- :: Maybe Umail)
+    --   (Left x) -> Left $ x
 
                      
 --authRequest :: Environment (IO ClientCredentials)--B.ByteString -> B.ByteString -> B.ByteString -> B.ByteString -> IO (Response B.ByteString)
@@ -255,12 +274,14 @@ main = do
                 secret  = "a503505ae803ee7f4fd477f01c1958b1"
              }
              
-  r <- authRequest client
-  print r
-  r2 <- umailGet r []
+  -- r <- authRequest client
+  -- print r
+  r2 <- umailGet client []
+  -- print $ r2
+  r3 <- apiGet client (("method", "umail.get"):[])
+  -- let rr = (\(Right x) -> x >>= (\t -> Just $ t & umail  )) r2
   print $ r2
-  r3 <- apiGet r (("method", "umail.get"):[])
-  print $ r3
+  print r3
   -- print $ r ^? responseBody . key "sid"
   -- -- (authorize appkey skey "hastest" "12341230") + 2
   -- a <- authorize appkey skey "hastest" "12341230"
