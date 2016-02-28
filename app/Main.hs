ddd8b29c4220610732393c49c428f549a298a607
capgelka <ldyach@yandex.ru>
2016-02-29 01:01:54 +0300

    recursive ununicode implementation

--- app/Main.hs
+++ app/Main.hs
@@ -14,9 +14,9 @@ import Data.Maybe (fromMaybe)
 import Data.Either
 import Data.Text (Text)
 import qualified Data.Text.Lazy as L (toStrict)
-import qualified Data.Text.Lazy.Encoding as LE (decodeLatin1, decodeUtf8)
+import qualified Data.Text.Lazy.Encoding as LE (decodeLatin1, decodeUtf8, encodeUtf8)
 import qualified Data.Text.Lazy.Builder as LB (toLazyText)
-import Data.Text.Encoding (decodeLatin1, decodeUtf8)
+import Data.Text.Encoding (decodeLatin1, decodeUtf8, encodeUtf8)
 import Data.Text.Read (decimal)
 import Data.Binary (encode)
 import qualified Data.ByteString.Base16 as B16 (encode)
@@ -33,6 +33,7 @@ import GHC.Generics (Generic)
 import qualified Control.Exception as E
 
 import Network.Wreq
+import Data.String.Unicode (unicodeToUtf8)
 
 
 
@@ -87,22 +88,22 @@ keyHash pass key = decodeLatin1 $ B16.encode $ MD5.hash $ B.append key pass --ne
 -- Get GHC to derive a FromJSON instance for us.
 
 -- apiGet :: IO (Response B.ByteString)
--- sid :: Reader
---ununicode :: BL.ByteString -> BL.ByteString                 -- 
-ununicode = BL.concat . (map (\x -> replace (BL.pack x))) . BL.unpack where -- sequence $ map (\x -> let elem = Map.lookup x table in 
---                               if elem /= Nothing then elem else Just x) s where
-  replace x = let elem = Map.lookup x table
-                in case (if elem /= Nothing then elem else Just x) of
-                    (Just a)  -> a
-                    -- (Nothing)  -> " "
+-- sid :: Reader (\x -> replace (BL.pack x))) 
+ununicode :: BL.ByteString -> BL.ByteString                 -- 
+ununicode s = replace s where 
+  replace :: BL.ByteString -> BL.ByteString
+  replace str = case (Map.lookup (BL.take 6 str) table) of
+          (Just x) -> BL.append x (replace $ BL.drop 6 str)
+          (Nothing) -> BL.cons (BL.head str)  (replace $ BL.tail str)
+
   table = Map.fromList $ zip letters rus
-  --rus =  "GGG" --[1040 .. 1103] :: [Integer]
+
   rus = ["Ё", "ё", "А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М",
          "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы",
          "Ь", "Э", "Ю", "Я", "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к",
          "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ",
-         "ъ", "ы", "ь", "э", "ю", "я"] :: [BL.ByteString]
-  --rus = "\1025\1105\1040\1041\1042\1043\1044\1045\1046\1047\1048\1049\1050\1051\1052\1053\1054\1055\1056\1057\1058\1059\1060\1061\1062\1063\1064\1065\1066\1067\1068\1069\1070\1071\1072\1073\1074\1075\1076\1077\1078\1079\1080\1081\1082\1083\1084\1085\1086\1087\1088\1089\1090\1091\1092\1093\1094\1095\1096\1097\1098\1099\1100\1101\1102\1103"
+         "ъ", "ы", "ь", "э", "ю", "я"]  :: [BL.ByteString]
+ 
   letters = ["\\u0401", "\\u0451", "\\u0410", "\\u0411", "\\u0412", "\\u0413", 
              "\\u0414", "\\u0415", "\\u0416", "\\u0417", "\\u0418", "\\u0419",
              "\\u041a", "\\u041b", "\\u041c", "\\u041d", "\\u041e", "\\u041f",
@@ -114,14 +115,9 @@ ununicode = BL.concat . (map (\x -> replace (BL.pack x))) . BL.unpack where -- s
              "\\u043e", "\\u043f", "\\u0440", "\\u0441", "\\u0442", "\\u0443",
              "\\u0444", "\\u0445", "\\u0446", "\\u0447", "\\u0448", "\\u0449",
              "\\u044a", "\\u044b", "\\u044c", "\\u044d", "\\u044e", "\\u044f"]
-  -- letters = "\\u0401\\u0451\\u0410\\u0411\\u0412\\u0413\\u0414\\u0415\\u0416\\\
-  --            \u0417\\u0418\\u0419\\u041a\\u041b\\u041c\\u041d\\u041e\\u041f\\\
-  --            \u0420\\u0421\\u0422\\u0423\\u0424\\u0425\\u0426\\u0427\\u0428\\\
-  --            \u0429\\u042a\\u042b\\u042c\\u042d\\u042e\\u042f\\u0430\\u0431\\\
-  --            \u0432\\u0433\\u0434\\u0435\\u0436\\u0437\\u0438\\u0439\\u043a\\\
-  --            \u043b\\u043c\\u043d\\u043e\\u043f\\u0440\\u0441\\u0442\\u0443\\\
-  --            \u0444\\u0445\\u0446\\u0447\\u0448\\u0449\\u044a\\u044b\\u044c\\\
-  --            \u044d\\u044e\\u044f"
+
+
+
 
 toOptions :: [(Text, Text)] -> Options
 toOptions x = defaults & foldr (\(x, y) p -> p . set (param x) [y]) id x -- (\x f-> defaults x . f)
@@ -158,7 +154,7 @@ umailGet env params = do
     x <- return $ case r of
           (Right a) -> a
           (Left a)  -> ""
-    return $ ununicode $ x
+    return $ LE.encodeUtf8 $ LE.decodeUtf8 $ x
     -- return $ case r of
     --   (Right x) -> Right $ (decode x :: Maybe Umail )-- >>= (\u -> u & umail >>= return)
     --              -- do :: Maybe
@@ -209,50 +205,6 @@ basic_asJSON = do
 
   putStrLn $ "args: " ++ show (args (r ^. responseBody))
 
-
-
--- The response we expect here is valid JSON, but cannot be converted
--- to an [Int], so this will throw a JSONError.
-
--- failing_asJSON :: IO ()
--- failing_asJSON = do
---   (r :: Response [Int]) <- asJSON =<< get "http://httpbin.org/get"
---   putStrLn $ "response: " ++ show (r ^. responseBody)
-
-
-
--- -- This demonstrates how to catch a JSONError.
-
--- failing_asJSON_catch :: IO ()
--- failing_asJSON_catch =
---   failing_asJSON `E.catch` \(e :: JSONError) -> print e
-
-
-
--- Because asJSON is parameterized over MonadThrow, we can use it with
--- other instances.
---
--- Here, instead of throwing an exception in the IO monad, we instead
--- evaluate the result as an Either:
---
--- * if the conversion fails, the Left constructor will contain
---   whatever exception describes the error
---
--- * if the conversion succeeds, the Right constructor will contain
---   the converted response
-
--- either_asJSON :: IO ()
--- either_asJSON = do
---   r <- get "http://httpbin.org/get"
-
---   -- This first conversion attempt will fail, but because we're using
---   -- Either, it will not throw an exception that kills execution.
---   let failing = asJSON r :: Either E.SomeException (Response [Int])
---   print failing
-
---   -- Our second conversion attempt will succeed.
---   let succeeding = asJSON r :: Either E.SomeException (Response GetBody)
---   print succeeding
 
 
 
@@ -321,10 +273,14 @@ main = do
 
   -- print letters
   r2 <- umailGet client []
-  -- print $ r2
-  r3 <- apiGet client (("method", "umail.get"):[])
+  -- -- print $ r2
+  -- r3 <- apiGet client (("method", "umail.get"):[])
   print $ r2
-  print r3
+  -- -- print r3
+  -- putStrLn $ BL.unpack r2
+  -- let p = "\\u0414\\u043e\\u0431\\u0440\\u043e \\u043f\\u043e\\u0436 Diary"
+  -- print p
+  -- putStrLn $ B.unpack $ ununicode' $ p
   -- print $ r ^? responseBody . key "sid"
   -- -- (authorize appkey skey "hastest" "12341230") + 2
   -- a <- authorize appkey skey "hastest" "12341230"
