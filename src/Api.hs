498ddbba2867da752f32ee8a1beef5b79f660ca3
capgelka <ldyach@yandex.ru>
2016-03-18 03:27:34 +0300

    try to implement function to get id by username

--- src/Api.hs
+++ src/Api.hs
@@ -12,8 +12,13 @@ module Api
 
 import qualified Data.ByteString.Lazy.Char8 as BL (ByteString)
 import Data.Text (Text)
+import qualified Data.Text.Lazy as L (Text)
 import Internal.Api (ClientCredentials(..), apiPost, authRequest)
+import Data.Aeson.Lens (key, _String)
+import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
 
+
+type Name = Text
 
 postCreate :: ClientCredentials -> [(Text, Text)] -> IO (Either Integer BL.ByteString)
 postCreate env p = apiPost env (("method", "post.create"):p)
@@ -28,3 +33,12 @@ umailGet env params = apiPost env (("method", "umail.get"):params)
 
 umailSend :: ClientCredentials -> [(Text, Text)] -> IO (Either Integer BL.ByteString)
 umailSend env params = apiPost env (("method", "umail.send"):("save_copy", "1"):params)
+
+
+nameById :: ClientCredentials -> Name -> IO (Maybe L.Text)
+nameById env name = do
+  response <- apiPost env [("method", "journal.get"), ("shortname", name)]
+  -- print <$> response . key "shortname" . _String
+  return $ case response of
+    (Right resp) -> Just $ decodeUtf8 $ resp -- . key "userid" -- . _String)
+    (Left _)     -> Nothing
