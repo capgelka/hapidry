24c37c725a293f52c08aa132750e813e97f69e2c
capgelka <ldyach@yandex.ru>
2017-01-04 02:34:51 +0300

    clean code in Main.hs. Fix output for hapidry comment

--- app/Main.hs
+++ app/Main.hs
@@ -5,20 +5,13 @@ import qualified Data.Configurator as C
 import Control.Lens ((&))
 import Api
 import Internal.Api (authRequest)
-import qualified Internal.Json as IJ--del
-import qualified Json as J
 
 import Options
 import Utils
 
-import Data.Maybe
-
-import Data.Aeson
 
 import Prelude hiding (putStr)
 import Data.ByteString.Char8 (putStr, pack)
-import qualified Data.Text.Encoding as E
--- import Data.ByteString.UTF8 (fromString)
 
 
 import qualified Data.Text    as T
@@ -30,17 +23,11 @@ import Data.Version (showVersion)
 import Development.GitRev (gitHash)
 import qualified Paths_hapidry as P (version)
 
-import System.Exit
 
 
 type Delimeter = T.Text
-extractP (IJ.PostList x) = x
-extractU (IJ.MessageList x) = x
-fromRight (Right x) = x
 
-
-
--- printResult :: Either BL.ByteString [BL.ByteString] -> Delimeter -> IO ()
+printResult :: Either BL.ByteString [BL.ByteString] -> Delimeter -> IO ()
 printResult (Left x)  _ = printError x
 printResult (Right xs) d = mapM_ (\x -> BL.putStr x >> T.putStr d) xs >> putStrLn ""
 
@@ -79,21 +66,7 @@ main = do
 
         parseOpt' p@Post {} client = (createPost p client) >>= \x -> return $ getResponseField "postid" x
         parseOpt' s@Send {} client = sendUmail s client >>= \x -> return $ getResponseField "message" x
-        parseOpt' c@Comment {} client = createComment c client -- >> mempty
+        parseOpt' c@Comment {} client = createComment c client >>= \x -> return $ getResponseField "message" x
         parseOpt' n@Notify {} client = getNotifications n client >> return (Right [""])
         parseOpt' p@Blog {} client = readPost p client >> return (Right [""])
         parseOpt' p@Umail {} client = readUmail p client >> return (Right [""])
-
-  -- parseOpt (command & commands) client >>= print where
-  -- parseOpt (command & commands) client >>= (\x -> BL.putStr $ fromRight x) where
-  -- parseOpt (command & commands) client >>= (T.putStr . IJ.messageHtml . head)  where
-  --     parseOpt p@Post {} client = createPost p client -- >> mempty
-  --     parseOpt s@Send {} client = sendUmail s client -- >> mempty
-  --     parseOpt c@Comment {} client = createComment c client -- >> mempty
-  --     parseOpt post client =  extractP <$> fromJust <$> postsFromJson <$> apiPost client [("method", "post.get"),
-  --                                                           ("type", "favorites")]
-      -- parseOpt _ client =  extractU <$> fromJust <$> umailsFromJson <$> (umailGet client [])
-      -- parseOpt _ client = (umailGet client [])
-
-      --parseOpt n@Notify {} client = getNotifications n client >> return (Right ["Ok"])
-   
