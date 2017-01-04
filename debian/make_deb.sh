cfec3e41643c8ca5a02d82a3757e73869d56f627
capgelka <ldyach@yandex.ru>
2017-01-04 19:02:58 +0300

    add debian packaging scripts

---
+++ debian/make_deb.sh
@@ -0,0 +1,44 @@
+MACHINE=$(uname -m)
+case "$MACHINE" in
+  x86_64) ARCHITECTURE=amd64;;
+  i686)   ARCHITECTURE=i386;;
+  i386)   ARCHITECTURE=i386;;
+esac
+
+LOCAL=$HOME/.local
+VERSION=$(grep -e '^version' hapidry.cabal | awk '{print $2}' | xargs)
+DEBVER=$VERSION
+BASE=hapidry-$DEBVER-$ARCHITECTURE
+DIST=`pwd`/$BASE
+DEST=$DIST/usr
+ME=$(whoami)
+COPYRIGHT=$DEST/share/doc/hapidry/copyright
+
+# We need this for hsb2hs:
+PATH=$LOCAL/bin:$PATH
+
+mkdir -p $DEST/bin
+mkdir -p $DEST/share/man/man1
+mkdir -p $DEST/share/doc/hapidry
+
+stack install --install-ghc --stack-yaml stack.yaml --local-bin-path . hsb2hs
+stack install --install-ghc --stack-yaml stack.yaml --local-bin-path . hapidry
+
+
+find $DIST -type d | xargs chmod 755
+cp $HOME/.local/bin/hapidry $DEST/bin/
+cp man/hapidry.1 $DEST/share/man/man1/hapidry.1
+gzip -9 $DEST/share/man/man1/hapidry.1
+
+INSTALLED_SIZE=$(du -B 1024 -s $DEST | awk '{print $1}')
+mkdir -p $DIST/DEBIAN
+perl -pe "s/VERSION/$DEBVER/" debian/control.in | \
+  perl -pe "s/ARCHITECTURE/$ARCHITECTURE/" | \
+  perl -pe "s/INSTALLED_SIZE/$INSTALLED_SIZE/" \
+  > $DIST/DEBIAN/control
+echo $DIST
+
+cat $DIST/DEBIAN/control
+
+fakeroot dpkg-deb --build $DIST
+rm -rf $DIST
